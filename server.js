d1690c605b462e8a100d6534942d4c558e915f9e
pwmurphy89 <pwmurphy89@gmail.com>
2016-06-20 12:04:58 -0400

    restructuring folder

--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var servStatic = require('serve-static');
-
-connect().use(servStatic(__dirname)).listen(8000, function(){
-	console.log('Listening on port 8000...');
-});
-
-
